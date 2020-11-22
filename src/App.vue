c98d2a0b4c351d880b8fe10ff6cd6fc14cbc78f3
Joshua MacSween <joshmacsween@gmail.com>
2020-11-22 14:50:21 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <todos :todos="todos" />
+    <add-todo v-on:add-todo="addTodo" />
+  </div>
+</template>
+
+<script>
+import AddTodo from './components/AddTodo.vue'
+import Todos from './components/Todos.vue'
+export default {
+  components: { Todos, AddTodo },
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Go workout',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Do laundry',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Cook food',
+          completed: false,
+        },
+        {
+          id: 4,
+          title: 'Clean up room',
+          completed: false,
+        },
+        {
+          i: 5,
+          title: 'Finish work',
+          completed: false,
+        },
+      ],
+    }
+  },
+  methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj]
+    },
+  },
+}
+</script>
+
+<style>
+  p {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  h1, h2, h3, h4, h5 {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>My Todo List</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for="todo in todos">
+        <todo v-bind:todo="todo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo'
+export default {
+  name: 'Todos',
+  components: {
+    Todo,
+  },
+  props: ["todos"]
+}
+</script>
+
+<style lang="scss" scoped></style>
